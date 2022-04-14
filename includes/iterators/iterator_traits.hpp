6b10a835d49f6dfa1e8590bf8cc45c0dd241ba9a
gchopin <gchopin@42.student.fr>
2022-04-14 17:39:32 +0200

    push iterator_traits.hpp

--- includes/iterators/iterator_traits.hpp
+++ includes/iterators/iterator_traits.hpp
@@ -1,6 +1,39 @@
 #ifndef ITERATOR_TRAITS_HPP
  # define ITERATOR_TRAITS_HPP
 
+#include	<cstddef>
 
+namespace ft
+{
+	template <class Iter>
+	class	iterator_traits
+	{
+		typename Iter::difference_type	difference_type;
+		typename Iter::value_type	value_type;
+		typename Iter::pointer		pointer;
+		typename Iter::reference		reference;
+		typename Iter::iterator_category	iterator_category;
+	};
+
+	template <typename T>
+	class	iterator_traits<T*>
+	{
+		typename std::ptrdiff_t	difference_type;
+		typedef T		value_type;
+		typedef T*		pointer;
+		typedef T&		reference;
+		//doit coder équivalent en ft:: std::random_access_iterator_tag	iterator_category;
+	};
+
+	template <typename T>
+	class	iterator_traits<const T*>
+	{
+		typename std::ptrdiff_t	difference_type;
+		typedef T		value_type;
+		typename const T*	pointer;
+		typename const T&	reference;
+		//doit coder équivalent en ft:: typename T::iterator_category	iterator_category;
+	};
+}
 
 #endif
